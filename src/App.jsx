e54fff00ccb3bdfba9d0b962bfd6b700b8c2c0a3
Julieta Gordillo <julietagordillo13@hotmail.com>
2023-11-01 17:46:05 -0300

    navbar component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./Styles/App.css";
+import Header from "./components/Header";
+
+function App() {
+  <div className="App-container">
+  <Header/>
+  return (
+   <h1>FVaccarezza</h1>
+      
+  )
+  </div>
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+const Header = () =>{
+    return (
+    <NavBar/>
+    )
+}
+export default Header;
