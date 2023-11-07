3816783b96c4c93811ff7d6c0f4a498cce715a53
Julieta Gordillo <julietagordillo13@hotmail.com>
2023-11-06 22:37:09 -0300

    button up style

--- src/components/ButtonUp.jsx
+++ src/components/ButtonUp.jsx
@@ -1,11 +1,9 @@
 import * as React from "react";
-import ButtonUpIcon from "../assets/img/ButtonUpIcon.svg";
+import ButtonUpIcon from "../assets/img/ButtonUpIcon.jsx";
 import "../styles/ButtonUp.css";
 const ButtonUp = () => (
   <button className="ButtonUp-container" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-    <svg className="ButtonUp" viewBox="0 0 50 50">
-      <path id="button-up-icon" d={ButtonUpIcon} />
-    </svg>
+<ButtonUpIcon/>
   </button>
 )
 export default ButtonUp;
