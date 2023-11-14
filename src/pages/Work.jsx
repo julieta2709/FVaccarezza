43717a1c772b0d3e5e34017eca3a6eed0c69b8df
Julieta Gordillo <julietagordillo13@hotmail.com>
2023-11-14 12:33:39 -0300

    Work styles

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,35 +1,58 @@
 import React from "react";
+import asterisk from "../assets/img/asterisk.svg";
+import "../styles/Work.css";
 const Work = () => {
   return (
     <div className="Work-container">
-      <h2>LATEST WORKS</h2>
-      <ul>
-        <li>
-          1 <span>FROG BAZAR.</span>
+      <div className="Work-TitleContainer">
+        <img className="Work-asterisk" src={asterisk} alt="asteriskk" />
+        <h2 className="Work-mainTitle">LATEST WORKS</h2>
+      </div>
+      <ul className="Work-List">
+        <li className="Work-ListItem">
+          <p className="Work-ItemNumber">
+            1 <span className="Work-ItemText">FROG BAZAR.</span>
+          </p>
         </li>
-        <li>
-          2 <span>ESTUDIO 32 -</span>
+        <li className="Work-ListItem">
+          <p className="Work-ItemNumber">
+            2 <span className="Work-ItemText">ESTUDIO 32 -</span>
+          </p>
         </li>
-        <li>
-          3 <span>N.MORENO/PEDRAZA</span>
+        <li className="Work-ListItem">
+          <p className="Work-ItemNumber">
+            3 <span className="Work-ItemText">N.MORENO/PEDRAZA</span>
+          </p>
         </li>
-        <li>
-          4 <span>OPAME COLLECTIVE =</span>
+        <li className="Work-ListItem">
+          <p className="Work-ItemNumber">
+            4 <span className="Work-ItemText">OPAME COLLECTIVE =</span>
+          </p>
         </li>
-        <li>
-          5 <span>MICHI BISTRO {`"`}</span>
+        <li className="Work-ListItem">
+          <p className="Work-ItemNumber">
+            5 <span className="Work-ItemText">MICHI BISTRO {`"`}</span>
+          </p>
         </li>
-        <li>
-          6 <span>BROKE MUSIC _</span>
+        <li className="Work-ListItem">
+          <p className="Work-ItemNumber">
+            6 <span className="Work-ItemText">BROKE MUSIC _</span>
+          </p>
         </li>
-        <li>
-          7 <span>KP INTERIOR DESIGN /</span>
+        <li className="Work-ListItem">
+          <p className="Work-ItemNumber">
+            7 <span className="Work-ItemText">KP INTERIOR DESIGN /</span>
+          </p>
         </li>
-        <li>
-          8 <span>LOGOFOLIO {`}`} </span>
+        <li className="Work-ListItem">
+          <p className="Work-ItemNumber">
+            8 <span className="Work-ItemText">LOGOFOLIO {`}`} </span>
+          </p>
         </li>
-        <li>
-          9 <span>3D MISC. ART *</span>
+        <li className="Work-ListItem">
+          <p className="Work-ItemNumber">
+            9 <span className="Work-ItemText">3D MISC. ART *</span>
+          </p>
         </li>
       </ul>
     </div>
