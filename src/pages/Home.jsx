d138d65fa93d245bf4c58ef5aabb00703b5774be
Julieta Gordillo <julietagordillo13@hotmail.com>
2023-11-13 23:30:27 -0300

    home styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,17 +6,15 @@ const Home = () => {
   return (
     <div className="Home-container">
       <div className="HomePF-Container">
-        <div className="redCircle-container">
         <div className="redCircle">
           <RedCircle />
-        </div>
-        <div className="redCircle">
-          <RedCircle />
-        </div>
         </div>
         <h1 className="portfolio">PORTFOLIO.</h1>
         <h2 className="year">2024</h2>
         <p className="fv">FEDERICO VACCAREZZA</p>
+        <div className="redCircle">
+          <RedCircle />
+        </div>
       </div>
     </div>
   );
